cf1a14efa31f38f92310787b0b2e40d81920f48c
Fearsest <109265550+Fearsest@users.noreply.github.com>
2022-08-14 10:01:10 +0300

    Add files via upload

--- source/options/BaseOptionsMenu.hx
+++ source/options/BaseOptionsMenu.hx
@@ -43,19 +43,13 @@ class BaseOptionsMenu extends MusicBeatSubstate
 
 	public var title:String;
 	public var rpcTitle:String;
-    public var newSound = FlxG.sound.playMusic(Paths.music('title'), 1, Bool);
-    override public function create()
-	{
-	    #if PRELOAD_ALL
-        newSound = true;
-	    #end
-
-        super.create();
-    }
 
 	public function new()
 	{
 		super();
+        #if PRELOAD_ALL
+        FlxG.sound.playMusic(Paths.music('title'), 1, true);
+        #end
 
 		if(title == null) title = 'Options';
 		if(rpcTitle == null) rpcTitle = 'Options Menu';
